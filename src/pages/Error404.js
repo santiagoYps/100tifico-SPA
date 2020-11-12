b999658f039519d01753827cd159cdac14cff7a0
Santiago Yepes <ysantiago@unicauca.edu.co>
2020-11-12 18:51:29 -0500

    Creadas templates de vistas

---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+const Error404 = () => {
+    return `
+    <div class="Error404">
+        <h2>
+            Error 404
+        </h2>
+    </div>
+    `;
+}
+
+export default Error404;
---
+++ src/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1 class="">
+                    <a href="/" class="">
+                    100tifi.co
+                    </a>
+                </h1>
+            </div>
+            <div class="Header-nav">
+                <a href="#/about">
+                About
+                </a>
+            </div>        
+        </div>
+    `;
+    return view;
+}
+
+export default Header;
